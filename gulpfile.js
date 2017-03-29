20e1f7fa02f91ed8051ab0cb3acb17e18526ecc2
Ale Paula <paulinhasell@gmail.com>
2017-03-29 13:41:45 -0300

    Ebook boostrap com Sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
+//compilar e gerar css a partir do scss
 var gulp = require("gulp");
 var sass = require ("gulp-sass");
 gulp.task ("compilar-css", function(){
 	return gulp.src("./source/scss/*.scss")
 	.pipe(sass())
 	.pipe(gulp.dest("./dist/css"))
+});
+//monitorar e atualizar atomaticamente alteracoes
+gulp.task ("escuta-me", function(){
+	gulp.watch("./source/scss/*.scss",["compilar-css"]);
 });
